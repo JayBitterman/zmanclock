e9927215bf69f2873a0647f91fc198d7570a3c61
Yaakov Bitterman <yaakovbitterman@gmail.com>
2026-01-11 20:21:57 -0500

    Fixed case sensitivity in import

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ import {
   getTimeAccelerationFactor
 } from './scene.js';
 import { fetchWeather, weatherState } from './weather.js';
-import { initLocation, locationState, getTimezoneForLocation } from './location.js';
+import { initLocation, locationState, getTimezoneForLocation } from './Location.js';
 
 // ============================================================================
 // Israel Detection (GeoJSON)
